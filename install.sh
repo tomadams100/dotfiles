939ea6329a8d90ad102f4d5d5aab126052a08a9b
Tom Adams <thomasjeadams@btinternet.com>
2022-04-27 16:08:57 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+apk add zsh curl wget git
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+apk add neovim
+
+cat .gitconfig > $HOME/.gitconfig
+
+npm install -g commitizen cz-conventional-changelog cz-customizable
+npm cache clean --force
+
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "             cloning zsh-syntax-highlighting               "
+    echo "-----------------------------------------------------------"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    echo "==========================================================="
+    echo "             cloning powerlevel10k                         "
+    echo "-----------------------------------------------------------"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo "==========================================================="
+    echo "             import zshrc                                  "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
+    echo "==========================================================="
+    echo "             import powerlevel10k                          "
+    echo "-----------------------------------------------------------"
+    cat .p10k.zsh > $HOME/.p10k.zsh
+}
+
+# change time zone
+# sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
+# sudo dpkg-reconfigure --frontend noninteractive tzdata
+
+zshrc
+
+# needs to be after zshrc
+echo "" >> ~/.zshrc
+echo "# remove ls and directory completion highlight color" >> ~/.zshrc
+echo "_ls_colors=':ow=01;33'" >> ~/.zshrc
+echo 'zstyle ":completion:*:default" list-colors "${(s.:.)_ls_colors}"' >> ~/.zshrc
+echo 'LS_COLORS+=$_ls_colors' >> ~/.zshrc
+echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
